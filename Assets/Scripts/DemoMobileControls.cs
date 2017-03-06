f491c0e5ec80954ffda4c8b291af18dae8ee5419
Robert Wells <robert.wells@gandhigames.co.uk>
2017-03-06 16:50:50 +0000

    added mouse controls for demo purposes

---
+++ Assets/Scripts/DemoMobileControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMobileControls : MonoBehaviour
+{
+    public DayNightController controller;
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var pos = Input.mousePosition;
+
+            if(pos.x > Screen.width * 0.5f)
+            { 
+                controller.CurrentTimeOfDay += 0.05f;
+            }
+            else
+            { 
+                controller.CurrentTimeOfDay -= 0.05f;
+            }
+        }
+    }
+}
